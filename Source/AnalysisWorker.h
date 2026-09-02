befe1cb2677f707985e4f937b336f385920c89f8
DebuNeko233 <101854723+DebuNeko233@users.noreply.github.com>
2026-09-03 03:40:46 +0800

    fix: track loudness cadence in samples

--- Source/AnalysisWorker.h
+++ Source/AnalysisWorker.h
@@ -132,7 +132,7 @@ private:
     float latestLufsIntegrated = -120.0f;
     float latestTruePeakDbtp = -120.0f;
     float maxTruePeakDbtp = -120.0f;
-    double lastLoudnessMetricsMs = 0.0;
+    std::uint64_t loudnessSamplesSinceMetrics = 0;
 
     bool signalPresent = false;
     float detectorPeakDb = -120.0f;
